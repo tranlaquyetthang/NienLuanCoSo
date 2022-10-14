6731939718c5a19afce74787812963c986cf0b70
Quyet Thang <b1909989quyetthang@gmail.com>
2022-10-14 20:22:15 +0700

    hoàn thành chức năng search sản phẩm

--- Project/frontend/src/App.js
+++ Project/frontend/src/App.js
@@ -3,6 +3,7 @@ import {
   Routes, // instead of "Switch"
   Route,
 } from "react-router-dom"
+import { useState } from "react"
 
 import { library } from '@fortawesome/fontawesome-svg-core'
 import {faSearch,faCartShopping,faLock,faKey,faSignIn,faUser,faEnvelope} from '@fortawesome/free-solid-svg-icons'
@@ -15,35 +16,23 @@ import Footer from "./Components/Public/Footer"
 import SignIn from "./Components/SignIn/SignIn"
 import Register from "./Components/Register/Register"
 
-import data from "./data/data"
-
 library.add(faSearch,faCartShopping,faLock,faKey,faEnvelope,faSignIn,faFacebookF,faTwitter,faLinkedinIn,faUser)
 
-function App() {
-  function callbackFunction(childData){
 
-    // data.Skills.map((skill)=>{
-    //   if(skill.Area===childData){
-    //     return console.log("Tim thay san pham")
-    //   }
-    //   else{
-    //     return console.log("Khong tim thay san pham")
-    //   }
-    // })
-    var check =data.Skills.find(e=>e.Area===childData)
-    if(typeof check ==='undefined'){
-      console.log("no")
-    }
-    else{
-      console.log("Yes")
-    }
+function App() {
+  //tim kiem san pham
+  const [valueTimKiem,setValueTK]=useState("")
+  const [searchParam]=useState(["tenSanPham"])
+  function setValueTimKiem(childData){
+    setValueTK(childData)
   }
+  //tim kiem san pham
   return (
     <div className="App">
        <Router>
-          <Header timKiem={callbackFunction} tuRoot={"hello"}/>
+          <Header getValue={setValueTimKiem} searchParam={valueTimKiem}/>
           <Routes>
-            <Route exact path="/" element={<Home />} />
+            <Route exact path="/" element={<Home kiemTra={valueTimKiem} searchParam={searchParam}/>} />
             <Route path="/SignIn.html" element={<SignIn/>} />
             <Route path="/Register.html" element={<Register/>} />
           </Routes>
--- Project/frontend/src/Components/Home/CartSanPham.jsx
+++ Project/frontend/src/Components/Home/CartSanPham.jsx
@@ -1,7 +1,7 @@
 function CartSanPham(props) {
     return (
         <div className="col-md-2 p-2 col-12">
-                    <img width="250px" height="232px" src={props.HinhAnh} alt="..." class="img-thumbnail"/>
+                    <img width="250px" height="232px" src={props.HinhAnh} alt="..." className="img-thumbnail"/>
                     <div>{props.NoiDung}</div>
                     <div>mua ngay/ them gio hang</div>
         </div>
